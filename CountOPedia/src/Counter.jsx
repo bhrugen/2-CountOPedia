a3c2b2250773b79eef8c1d0b3698c00563cced33
bhrugen <bhrugen1990@gmail.com>
2025-07-04 15:07:42 -0500

    Section 4 - UI Updates

--- CountOPedia/src/Counter.jsx
+++ CountOPedia/src/Counter.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-
+import attackImg from "./images/attack.png";
+import defenceImg from "./images/defend.png";
 function Counter() {
   let [count, setCount] = useState(0);
 
@@ -7,23 +8,49 @@ function Counter() {
     setCount(function (prev) {
       return prev + 1;
     });
-
-    console.log(count);
   }
   function handleDecrement() {
     setCount((prev) => prev - 1);
-    console.log(count);
   }
 
   return (
-    <div className="row text-white container">
-      Counter : {count}
-      <button onClick={handleIncrement} className="btn btn-success m-2">
-        +1
-      </button>
-      <button onClick={handleDecrement} className="btn btn-danger m-2">
-        -1
-      </button>
+    <div className="container">
+      <div className="row text-white text-center ">
+        <h1> Game Score : {count}</h1>
+        <p>You win at +5 points and lose at -5 points!</p>
+
+        <h3>Game Status : </h3>
+
+        <div className="col-6 col-md-3 offset-md-3">
+          <img
+            src={attackImg}
+            style={{
+              width: "100%",
+              cursor: "pointer",
+              border: "1px solid green",
+            }}
+            className="p-4 rounded"
+          ></img>
+        </div>
+        <div className="col-6 col-md-3 offset-md-3">
+          <img
+            src={defenceImg}
+            style={{
+              width: "100%",
+              cursor: "pointer",
+              border: "1px solid red",
+            }}
+            className="p-4 rounded"
+          ></img>
+        </div>
+
+        <button onClick={handleIncrement} className="btn btn-success m-2">
+          +1
+        </button>
+        <button onClick={handleDecrement} className="btn btn-danger m-2">
+          -1
+        </button>
+      </div>
     </div>
   );
 }
