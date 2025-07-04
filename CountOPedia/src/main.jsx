7815706bd61428c5256f7bbb0a06e61a59242c35
bhrugen <bhrugen1990@gmail.com>
2025-07-04 12:40:50 -0500

    Section 4 - Header and Footer Component

--- CountOPedia/src/main.jsx
+++ CountOPedia/src/main.jsx
@@ -1,6 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import Header from "./Layout/Header";
+import Footer from "./Layout/Footer";
 createRoot(document.getElementById("root")).render(
-  <StrictMode>Hello World</StrictMode>
+  <StrictMode>
+    <div className="d-flex flex-column min-vh-100">
+      <Header />
+      <main className="flex-fill"></main>
+      <Footer />
+    </div>
+  </StrictMode>
 );
