b4c81bb8bb096c825dc45baf4d268ca96d024541
bhrugen <bhrugen1990@gmail.com>
2025-07-04 13:52:06 -0500

    Section 4 - Counter Component

--- CountOPedia/src/main.jsx
+++ CountOPedia/src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Header from "./Layout/Header";
 import Footer from "./Layout/Footer";
+import Counter from "./Counter";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <div className="d-flex flex-column min-vh-100">
       <Header />
-      <main className="flex-fill"></main>
+      <main className="flex-fill">
+        <Counter></Counter>
+      </main>
       <Footer />
     </div>
   </StrictMode>
